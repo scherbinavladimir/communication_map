6ba9055b99945bdf9ec7d2e2719fd3b9c2d5dd47
Щербина Владимир Владимирович (4097449) <vvscherbina@vtb.ru>
2026-02-21 11:41:08 +0300

    update

--- communications_map/settings.py
+++ communications_map/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r(*)74f6jn)!tf)h&m0x3y^vhx96qlh*f6$m1_+mhldstz0i18'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', '') == '1'
 
 ALLOWED_HOSTS = ['*']
 
